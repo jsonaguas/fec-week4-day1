9e9f84bfe52e071f8e1b4514b8039cba38c19265
jsonaguas <jaguas43@gmail.com>
2024-08-12 17:45:58 -0500

    upload

---
+++ Problem1.js
@@ -0,0 +1,19 @@
+
+let name = "Jimmy";
+let age = 20;
+let grade = "junior";
+let attendance = "present";
+
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Grade: " + grade);
+console.log("Attendance: " + attendance);
+
+const Jimmy = {
+    name: "Jimmy",
+    age: 20,
+    grade: "junior",
+    attendance: "present"
+};
+console.log(Jimmy);
---
+++ Problem2.js
@@ -0,0 +1,45 @@
+let var1 = 10;
+let var2 = 5;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log("Addition: " + add(var1, var2));
+
+var1 = 20;
+var2 = 10;
+console.log(var1); // Checks if var1 was assigned new value of 20//
+
+console.log("Subtraction: " + subtract(var1, var2));
+console.log("Multiplication: " + multiply(var1, var2));
+console.log("Division: " + divide(var1, var2));
+
+
+
+equal = var1 == var2
+console.log("Is num1 equal to num2?: " + equal);
+
+greater = var1 > var2
+console.log("Is num1 greater than num2?: " + greater);
+
+notEqual = var1 != var2
+console.log("Is num1 not equal to num2?: " + notEqual);
+
+positiveNumbers = var1 > 0 && var2 > 0
+console.log("Are both numbers positive?: " + positiveNumbers);
+
+onePositive = var1 > 0 || var2 > 0
+console.log("Is at least one number positive?: " + onePositive);
